fb1f2e0d1344bdcc62d9d67b8c88f3da6d23f3d3
Manuel Salvadori <morke.bm@hotmail.it>
2026-05-17 17:54:59 +0200

    Added options page

--- app/[locales]/options/page.tsx
+++ app/[locales]/options/page.tsx
@@ -1,15 +1,19 @@
 import UnitsForm from "@/components/unitsForm/unitsForm";
 import { defaultUnits, Units } from "@/utils/utils";
 import { cookies } from "next/headers";
+import styles from "./page.module.css";
+import { getTranslations } from "next-intl/server";
 
 export default async function Page() {
+    const t = await getTranslations("Options");
     const cookieStore = await cookies();
     const unitsRaw = cookieStore.get("units")?.value;
     const units: Units = unitsRaw ? JSON.parse(decodeURIComponent(unitsRaw)) : defaultUnits;
 
     return (
-        <div>
-            OPTIONS: <UnitsForm key={JSON.stringify(units)} units={units} />
+        <div className={styles.body}>
+            <h1>{t("options")}</h1>
+            <UnitsForm key={JSON.stringify(units)} units={units} />
         </div>
     );
 }
--- components/unitsForm/unitsForm.tsx
+++ components/unitsForm/unitsForm.tsx
@@ -3,9 +3,12 @@
 import { setUnitsAction } from "@/app/actions/setUnitsAction";
 import { useRouter } from "@/i18n/navigation";
 import { Units } from "@/utils/utils";
+import styles from "./unitsForm.module.css";
+import { useTranslations } from "next-intl";
 
 export default function UnitsForm({ units }: { units: Units }) {
     const router = useRouter();
+    const t = useTranslations("Options");
 
     const handleSubmit = async (formData: FormData) => {
         const payload: Units = {
@@ -19,23 +22,34 @@ export default function UnitsForm({ units }: { units: Units }) {
     };
 
     return (
-        <form action={handleSubmit}>
-            <select name='precipitation' defaultValue={units.precipitation}>
-                <option value='mm'>mm</option>
-                <option value='inch'>inch</option>
-            </select>
+        <form action={handleSubmit} className={styles.body}>
+            <div>
+                <label htmlFor='precipitation'>{t("prepUnit")}</label>
+                <select name='precipitation' defaultValue={units.precipitation}>
+                    <option value='mm'>mm</option>
+                    <option value='inch'>inch</option>
+                </select>
+            </div>
 
-            <select name='temperature' defaultValue={units.temperature}>
-                <option value='celsius'>celsius</option>
-                <option value='fahrenheit'>fahrenheit</option>
-            </select>
+            <div>
+                <label htmlFor='temperature'>{t("tempUnit")}</label>
+                <select name='temperature' defaultValue={units.temperature}>
+                    <option value='celsius'>celsius</option>
+                    <option value='fahrenheit'>fahrenheit</option>
+                </select>
+            </div>
 
-            <select name='wind_speed' defaultValue={units.wind_speed}>
-                <option value='kmh'>km/h</option>
-                <option value='mph'>mph</option>
-            </select>
+            <div>
+                <label htmlFor='wind_speed'>{t("windUnit")}</label>
+                <select name='wind_speed' defaultValue={units.wind_speed}>
+                    <option value='kmh'>km/h</option>
+                    <option value='mph'>mph</option>
+                </select>
+            </div>
 
-            <button type='submit'>SAVE</button>
+            <button type='submit' className={styles.submitButton}>
+                SAVE
+            </button>
         </form>
     );
 }
